05c0e073e19554df534fa8093330ab2a591df7e2
Benjamin Kettor Jr <bkettor@ucsd.edu>
2026-04-27 14:10:18 -0700

    Finished Lab 4 DevTools section

---
+++ explore/devtools/fix.js
@@ -0,0 +1,11 @@
+function printSum() {
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+
+  document.getElementById("sum").innerHTML = "Sum: " + calculateSum(num1, num2);
+}
+
+function calculateSum(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
